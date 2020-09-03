6550bd165e0e5af43de6aa6248d1bf695caadbf0
Rafnix Guzman <rafnixg@gmail.com>
2020-09-03 17:34:44 -0500

    [ADD] Iluminar usando la secuencia

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ class Juego {
   constructor() {
     this.inicializar();
     this.generarSecuencia();
+    this.siguienteNivel();
   }
 
   inicializar() {
     btnEmpezar.classList.add("hide");
     this.nivel = 1;
     this.colores = {
-        celeste,
-        violeta,
-        naranja,
-        verde
-    }
+      celeste,
+      violeta,
+      naranja,
+      verde,
+    };
   }
 
   generarSecuencia() {
@@ -26,8 +27,39 @@ class Juego {
       .fill(0)
       .map((n) => Math.floor(Math.random() * 4));
   }
+
+  siguienteNivel() {
+    this.iluminarSecuencia();
+  }
+
+  transformarNumeroAColor(numero) {
+    switch (numero) {
+      case 0:
+        return "celeste";
+      case 1:
+        return "violeta";
+      case 2:
+        return "naranja";
+      case 3:
+        return "verde";
+    }
+  }
+
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      const color = this.transformarNumeroAColor(this.secuencia[i]);
+      setTimeout(() => this.iluminarColor(color), 1000*i);
+    }
+  }
+  iluminarColor(color) {
+    this.colores[color].classList.add("light");
+    setTimeout(() => this.apagarColor(color), 350);
+  }
+  apagarColor(color) {
+    this.colores[color].classList.remove("light");
+  }
 }
 
 function empezarJuego() {
-  window.juego = new Juego();
+  window.juego = new Juego(); // Debug
 }
